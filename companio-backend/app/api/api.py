ef3a2a8fdbb9c9f57849a0c7df44bcdc11461e8d
Leonardo Vida <leonardo.vida@beerwulf.com>
2023-04-16 23:04:02 +0200

    Update

--- companio-backend/app/api/api.py
+++ companio-backend/app/api/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 # from app.api.endpoints import old_routes as notes
-from app.api.endpoints import auth, lists, users
+from app.api.endpoints import auth, lists, test, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(lists.router, prefix="/lists", tags=["lists"])
+api_router.include_router(test.router, prefix="/test", tags=["test"])
 # api_router.include_router(notes.router, prefix="/notes", tags=["notes"])
