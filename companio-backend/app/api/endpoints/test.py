e776f234211f255d6b925e2d4159e60ed3122972
Leonardo Vida <leonardo.vida@prima.it>
2023-07-22 17:00:51 +0200

    Rename folders

--- companio-backend/app/api/endpoints/test.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/hello_world")
-async def hello_world():
-    """Get current user"""
-    return {"message": "Hello World. Welcome to FastAPI!"}
--- companio-devops/vars.example.sh
+++
@@ -1,7 +0,0 @@
-export USER=ubuntu
-export HOST=123.456.789.10
-export DOMAIN=example.com
-export DB_USER=companio_boilerplate_user
-export DB_PASSWORD=password
-export DB_NAME=companio_boilerplate_dev
-export SSH_KEY_PATH=key.pem
